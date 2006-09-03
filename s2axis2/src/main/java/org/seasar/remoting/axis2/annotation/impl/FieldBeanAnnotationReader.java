78c097d4f47e067580da4ac52d5f021fee47bb4b
takanori <takanori@819c93ab-f40a-0410-9089-b01892e1e023>
2006-09-03 13:55:09 +0000

    [S2Axis2]JIRA AXIS-5
    git-svn-id: https://www.seasar.org/svn/s2axis/trunk@90 819c93ab-f40a-0410-9089-b01892e1e023

---
+++ s2axis2/src/main/java/org/seasar/remoting/axis2/annotation/impl/FieldBeanAnnotationReader.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.remoting.axis2.annotation.impl;
+
+import java.lang.reflect.Field;
+import org.seasar.framework.beans.BeanDesc;
+import org.seasar.framework.beans.PropertyDesc;
+import org.seasar.framework.beans.factory.BeanDescFactory;
+import org.seasar.framework.util.FieldUtil;
+import org.seasar.remoting.axis2.annotation.BeanAnnotationReader;
+
+public class FieldBeanAnnotationReader implements BeanAnnotationReader {
+
+    public String    PARAMETER_SUFFIX = "_PARAMETER";
+
+    private BeanDesc beanDesc;
+
+    public FieldBeanAnnotationReader(Class beanClass_) {
+        this.beanDesc = BeanDescFactory.getBeanDesc(beanClass_);
+    }
+
+    public String getParameterAnnotation(PropertyDesc pd) {
+        String propertyName = pd.getPropertyName();
+        String parameterNameKey = propertyName + PARAMETER_SUFFIX;
+        if (beanDesc.hasField(parameterNameKey)) {
+            Field field = beanDesc.getField(parameterNameKey);
+            return (String) FieldUtil.get(field, null);
+        }
+        return null;
+    }
+}
--- s2axis2/src/main/java/org/seasar/remoting/axis2/connector/RESTConnector.java
+++ s2axis2/src/main/java/org/seasar/remoting/axis2/connector/RESTConnector.java
@@ -22,7 +22,6 @@ import java.net.URL;
 import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.Map;
-
 import org.apache.axiom.om.OMElement;
 import org.apache.axis2.AxisFault;
 import org.apache.axis2.Constants;
@@ -31,28 +30,60 @@ import org.apache.axis2.client.Options;
 import org.apache.axis2.client.RESTCall;
 import org.apache.axis2.databinding.utils.BeanUtil;
 import org.apache.commons.beanutils.BeanUtils;
+import org.seasar.extension.dxo.converter.impl.SetConverter;
+import org.seasar.framework.beans.BeanDesc;
+import org.seasar.framework.beans.PropertyDesc;
+import org.seasar.framework.beans.factory.BeanDescFactory;
+import org.seasar.framework.container.S2Container;
+import org.seasar.remoting.axis2.annotation.AnnotationReaderFactory;
+import org.seasar.remoting.axis2.annotation.BeanAnnotationReader;
+import org.seasar.remoting.axis2.annotation.impl.AnnotationReaderFactoryImpl;
 import org.seasar.remoting.axis2.util.OMElementUtil;
 import org.seasar.remoting.axis2.xml.OMElementDeserializer;
 import org.seasar.remoting.axis2.xml.XMLBindException;
 import org.seasar.remoting.common.connector.impl.TargetSpecificURLBasedConnector;
 
 /**
+ * RESTをサポートしているWebサービスと通信するためのコネクタです。
  * 
  * @author takanori
- * 
  */
 public class RESTConnector extends TargetSpecificURLBasedConnector {
 
-    private static final String REST_GET_OPERATION  = "get";
+    /** GETを行う際の指定子 */
+    private static final String     REST_GET_OPERATION      = "get";
 
-    private static final String REST_POST_OPERATION = "post";
+    /** POSTを行う際の指定子 */
+    private static final String     REST_POST_OPERATION     = "post";
 
-    private String              encode              = OMElementUtil.DEFAULT_ENCODE;
+    /** エンコード */
+    private String                  encode                  = OMElementUtil.DEFAULT_ENCODE;
 
-    private Map                 deserializerMap     = new HashMap();
+    /** RESTのメッセージを生成するデシアライザのマップ */
+    private Map                     deserializerMap         = new HashMap();
 
+    private S2Container             container;
+
+    private AnnotationReaderFactory annotationReaderFactory = new AnnotationReaderFactoryImpl();
+
+    private BeanAnnotationReader    reader;
+
+    /**
+     * デフォルトのコンストラクタ。
+     */
     public RESTConnector() {}
 
+    /**
+     * RESTサービスを呼び出します。
+     * 
+     * @param url
+     *            呼び出し先のURL
+     * @param method
+     *            呼び出し先のメソッド
+     * @param args
+     *            リクエストに設定するパラメータ
+     * @return RESTサービスの呼び出し結果
+     */
     protected Object invoke(URL url, Method method, Object[] args)
             throws Throwable {
 
@@ -73,19 +104,25 @@ public class RESTConnector extends TargetSpecificURLBasedConnector {
         return result;
     }
 
+    /**
+     * REST形式の呼び出しを行う際のAxis2のOptionを生成します。
+     * 
+     * @param methodName
+     *            メソッド名
+     * @return Axis2のオプション
+     */
     private Options createOptions(String methodName) {
         Options options = new Options();
         options.setTransportInProtocol(Constants.TRANSPORT_HTTP);
         options.setProperty(Constants.Configuration.ENABLE_REST,
-                            Constants.VALUE_TRUE);
+                Constants.VALUE_TRUE);
         options.setProperty(Constants.Configuration.ENABLE_REST_THROUGH_GET,
-                            Constants.VALUE_TRUE);
+                Constants.VALUE_TRUE);
 
         String opeProp;
         if (methodName.toLowerCase().startsWith(REST_POST_OPERATION)) {
             opeProp = Constants.Configuration.HTTP_METHOD_POST;
-        }
-        else {
+        } else {
             opeProp = Constants.Configuration.HTTP_METHOD_GET;
         }
         options.setProperty(Constants.Configuration.HTTP_METHOD, opeProp);
@@ -93,49 +130,76 @@ public class RESTConnector extends TargetSpecificURLBasedConnector {
         return options;
     }
 
+    /**
+     * RESTリクエストを生成します。
+     * 
+     * @param args
+     *            リクエストに設定するパラメータ
+     * @return RESTリクエスト（指定するURL）
+     * @throws IllegalArgumentException
+     * @throws IllegalAccessException
+     * @throws InvocationTargetException
+     * @throws NoSuchMethodException
+     * @throws UnsupportedEncodingException
+     */
     protected String createTarget(Object[] args)
-            throws IllegalArgumentException,
-                IllegalAccessException,
-                InvocationTargetException,
-                NoSuchMethodException,
-                UnsupportedEncodingException {
+            throws IllegalArgumentException, IllegalAccessException,
+            InvocationTargetException, NoSuchMethodException,
+            UnsupportedEncodingException {
 
         String target;
 
         if (args == null || args.length <= 0) {
             target = this.baseURL.toString();
-        }
-        else if (args.length == 1) {
+        } else if (args.length == 1) {
             StringBuffer buff = new StringBuffer(this.baseURL.toString());
             buff.append("?");
             buff.append(createRestParameter(args[0]));
 
             target = buff.toString();
-        }
-        else {
-            throw new IllegalArgumentException("argument num is too much. it must be 0 or 1.");
+        } else {
+            throw new IllegalArgumentException(
+                    "argument num is too much. it must be 0 or 1.");
         }
 
         return target;
     }
 
+    /**
+     * RESTリクエストを作成します。
+     * 
+     * @param bean
+     *            パラメータを保持するオブジェクト
+     * @return RESTリクエスト
+     * @throws IllegalAccessException
+     * @throws InvocationTargetException
+     * @throws NoSuchMethodException
+     * @throws UnsupportedEncodingException
+     */
     private String createRestParameter(Object bean)
-            throws IllegalAccessException,
-                InvocationTargetException,
-                NoSuchMethodException,
-                UnsupportedEncodingException {
+            throws IllegalAccessException, InvocationTargetException,
+            NoSuchMethodException, UnsupportedEncodingException {
+
         Map paramMap = BeanUtils.describe(bean);
 
-        StringBuffer buff = new StringBuffer();
+        this.reader = createBeanAnnotationReader(bean.getClass());
+        BeanDesc beanDesc = BeanDescFactory.getBeanDesc(bean.getClass());
+        for (int i = 0; i < beanDesc.getPropertyDescSize(); i++) {
+            PropertyDesc pd = beanDesc.getPropertyDesc(i);
+            String key = pd.getPropertyName();
+            Object value = paramMap.get(key);
+            String newKey = getParameterName(pd);
+            paramMap.remove(key);
+            paramMap.put(newKey, value);
+        }
 
+        StringBuffer buff = new StringBuffer();
         Object[] keys = paramMap.keySet().toArray();
         for (int i = 0; i < keys.length; i++) {
-
             Object key = keys[i];
             Object value = paramMap.get(key);
 
             if ((!key.equals("class")) && (value != null)) {
-
                 if (i > 0) {
                     buff.append("&");
                 }
@@ -151,6 +215,60 @@ public class RESTConnector extends TargetSpecificURLBasedConnector {
         return restParam.toString();
     }
 
+    /**
+     * BeanAnnotationReaderを生成します。
+     * 
+     * @param bean
+     * @return BeanAnnotationReader
+     */
+    private BeanAnnotationReader createBeanAnnotationReader(Class bean) {
+        BeanAnnotationReader reader;
+
+        if (this.annotationReaderFactory == null) {
+            this.annotationReaderFactory = (AnnotationReaderFactory) this.container.getComponent(AnnotationReaderFactory.class);
+        }
+
+        if (this.annotationReaderFactory != null) {
+            reader = this.annotationReaderFactory.createBeanAnnotationReader(bean);
+        } else {
+            reader = null;
+        }
+
+        return reader;
+    }
+
+    /**
+     * RESTリクエストで指定するパラメータ名を取得します。 <br>
+     * アノテーションが指定されている場合は、そのパラメータを取得します。
+     * 
+     * @param propertyDesc
+     * @return パラメータ名
+     */
+    private String getParameterName(PropertyDesc propertyDesc) {
+        String ca;
+
+        if (this.reader != null) {
+            ca = this.reader.getParameterAnnotation(propertyDesc);
+        } else {
+            ca = null;
+        }
+
+        if (ca == null) {
+            ca = propertyDesc.getPropertyName();
+        }
+        return ca;
+    }
+
+    /**
+     * OMElemnt をデシアライズします。
+     * 
+     * @param returnType
+     *            戻り値の型
+     * @param om
+     *            OMElemnt
+     * @return 戻り値
+     * @throws XMLBindException
+     */
     private Object deserialize(Class returnType, OMElement om)
             throws XMLBindException {
 
@@ -161,20 +279,16 @@ public class RESTConnector extends TargetSpecificURLBasedConnector {
         if (deserializer != null) {
             // Bind by deserializer
             result = deserializer.deserialize(om);
-        }
-        else if (returnType.equals(String.class)) {
+        } else if (returnType.equals(String.class)) {
             // Convert to String
             result = OMElementUtil.toString(om);
-        }
-        else if (returnType.isAssignableFrom(OMElement.class)) {
+        } else if (returnType.isAssignableFrom(OMElement.class)) {
             result = om;
-        }
-        else {
+        } else {
             // Bind by BeanUtil(expect simple JavaBeans)
             try {
                 result = BeanUtil.deserialize(returnType, om);
-            }
-            catch (AxisFault ex) {
+            } catch (AxisFault ex) {
                 throw new XMLBindException(ex);
             }
         }
@@ -192,6 +306,10 @@ public class RESTConnector extends TargetSpecificURLBasedConnector {
 
     public void setEncode(String encode) {
         this.encode = encode;
+    }
+
+    public void setContainer(S2Container container) {
+        this.container = container;
     }
 
 }
